a32e6478ef8b1e01f753237fea95c8f15ecee90d
Sarabjeet Singh <88402524+Happynamikaze@users.noreply.github.com>
2023-09-16 00:13:20 +0530

    section 1 and 2 cmplt last left

---
+++ main.js
@@ -0,0 +1,19 @@
+$('.label').click(function(){debugger;
+	$(this).toggleClass("checked")
+	
+	
+	});
+	function formatState (state) {
+		if (!state.id) {
+		  return state.text;
+		}
+		var baseUrl = "/user/pages/images/flags";
+		var $state = $(
+		  '<span><img src="' + baseUrl + '/' + state.element.value.toLowerCase() + '.png" class="img-flag" /> ' + state.text + '</span>'
+		);
+		return $state;
+	  };
+	  
+	  $(".js-example-templating").select2({
+		templateResult: formatState
+	  });
